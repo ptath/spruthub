fd30748f9b701767a45299166537792dc8802e36
Арсений Шестаков <ptath@ptath.ru>
2019-03-03 00:40:14 +0400

    Исправлем ошибки

--- sprt_cfg_w.sh
+++ sprt_cfg_w.sh
@@ -9,7 +9,7 @@ sprutcfg_json_write()
 {
         S_JSON_KEY="$1"
         S_JSON_VALUE="$2"
-        jq --arg key "$S_JSON_KEY" --arg value "$S_JSON_VALUE" '. + {($key): ($value)}' ~/spru$
+        jq --arg key "$S_JSON_KEY" --arg value "$S_JSON_VALUE" '. + {($key): ($value)}' ~/spruthub/spruthub.json
 }
 
 sprutcfg_json_write $1 $2
