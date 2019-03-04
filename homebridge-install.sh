00bc5f11397e817f1083b428f351166fbf037906
Арсений Шестаков <ptath@ptath.ru>
2019-03-05 01:55:46 +0400

    Проверка установки npm-пакетов и возможность переустановки

--- homebridge-install.sh
+++ homebridge-install.sh
@@ -40,7 +40,7 @@ case $PI_ARM_VERSION in
       echo $NODE_VERSION |
       egrep 'v([0-9]+\.?){3}' -o
     );
-    echo "=== > Последняя доступная версия $VERSION"
+    echo "=== > Последняя доступная версия: $VERSION"
 
     #get lts version
     LTS_VERSION=$(echo $NODE_VERSION |
@@ -53,11 +53,11 @@ case $PI_ARM_VERSION in
     read -t 10 -n 1 -p "=== Установить LTS (Stable, рекомендуется) или последнюю $VERSION (Latest)? (S/l): " version_choice
     [ -z "$version_choice" ] && version_choice="s"
     case $version_choice in
-            s|S )
+            s|S|* )
               echo " Ставим LTS" &&
               script_url="https://github.com/sdesalas/node-pi-zero/raw/master/install-node-v.lts.sh"
             ;;
-            l|L|* )
+            l|L )
               echo " Ставим $VERSION" &&
               script_url="https://github.com/sdesalas/node-pi-zero/raw/master/install-node-v.last.sh"
             ;;
@@ -69,7 +69,7 @@ case $PI_ARM_VERSION in
       /tmp/install-node.sh &&
       rm /tmp/install-node.sh
 
-      if [ $(cat ~/.profile 2>/dev/null | grep -c "export PATH=\$PATH:/opt/nodejs/bin") -eq 0 ];then
+    if [ $(cat ~/.profile 2>/dev/null | grep -c "export PATH=\$PATH:/opt/nodejs/bin") -eq 0 ];then
       echo "=== > Добавляем /opt/nodejs/bin в PATH ~/.profile"
       echo "export PATH=\$PATH:/opt/nodejs/bin" >> ~/.profile
     else
@@ -119,6 +119,7 @@ done
 # echo "=== Устанавливаем (обновляем) менеджер пакетов npm"
 echo "=== Установка Homebridge и дополнительных пакетов Node.js"
 
+# Список пакетов
 npm_packages2install=(
 homebridge
 homebridge-config-ui-x
@@ -126,15 +127,32 @@ homebridge-zigbee
 pm2
 )
 
+[ -e /tmp/npm_installed_list ] &&
+  rm /tmp/npm_installed_list &&
+  echo $(npm list -g --depth 0) > /tmp/npm_installed_list
+
 for item in ${npm_packages2install[*]}
 do
   package_name=$item
-  if [ $(npm list -g --depth 0 | grep -c "$package_name@" 2>/dev/null) -eq 0 ];then
-        echo "=== >"$ package_name" не установлен, ставим..."
-        npm install -g "$package_name"
+  if [ $(cat /tmp/npm_installed_list 2>/dev/null | grep -c "$package_name@") -eq 0 ];then
+    echo "=== >"$ package_name" не установлен, ставим..."
+    npm install -g "$package_name"
   else
-        echo "=== >" $package_name" уже установлен"
+    read -t 10 -n 1 -p "=== >" $package_name" уже установлен, переустановить? (N/y): " reinstall_choice
+    [ -z "$reinstall_choice" ] && reinstall_choice="n"
+    case $version_choice in
+            y|Y )
+              echo " Переустанавливаем..." &&
+              npm remove -g "$package_name"
+              npm install -g "$package_name"
+            ;;
+            n|N|* )
+              echo " Оставляем" &&
+            ;;
+    esac
   fi
 done
+
+#
 
 #wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v.lts.sh | bash
